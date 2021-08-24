0a749f0b6bc855150006e02ea9f3d09a732bde52
Matheus Laurindo <matheus104521@gmail.com>
2021-08-24 18:18:07 -0300

    Formata os documentos com Prettier

--- src/script.js
+++ src/script.js
@@ -1,15 +1,15 @@
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 
 function createProductItemElement(product) {
-  const image = document.createElement('img');
-  image.setAttribute('src', product.image)
+  const image = document.createElement("img");
+  image.setAttribute("src", product.image);
   return image;
 }
 
 function getInfosApi(object) {
   const infos = object.top.map((element) => ({
-     image: element.image_url,
-    }));
+    image: element.image_url,
+  }));
   return infos.forEach((product) => {
     body.appendChild(createProductItemElement(product));
   });
@@ -18,25 +18,25 @@ function getInfosApi(object) {
 const messageError = (error) => console.log(error.message);
 
 async function getNameAnimeOrManga(type, name) {
-  const url = `https://api.jikan.moe/v3/search/${type}?q=${name}&page=1`
+  const url = `https://api.jikan.moe/v3/search/${type}?q=${name}&page=1`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    return data.results.forEach((element) => {  
-      const anime = (element);
-  });
+    return data.results.forEach((element) => {
+      const anime = element;
+    });
   } catch (error) {
     messageError(error);
   }
 }
 
 async function fetchApiAnime() {
-  const url = 'https://api.jikan.moe/v3/top/anime/1/favorite'
+  const url = "https://api.jikan.moe/v3/top/anime/1/favorite";
 
   try {
     const response = await fetch(url);
-    const data = await response.json()
+    const data = await response.json();
     getInfosApi(data);
   } catch (error) {
     messageError(error);
@@ -44,21 +44,20 @@ async function fetchApiAnime() {
 }
 
 async function fetchApiManga() {
-  const url = 'https://api.jikan.moe/v3/top/manga/1/favorite'
+  const url = "https://api.jikan.moe/v3/top/manga/1/favorite";
 
   try {
     const response = await fetch(url);
-    const data = await response.json()
+    const data = await response.json();
     // getInfosApi(data);
   } catch (error) {
     messageError(error);
   }
 }
 
-
 window.onload = () => {
-  fetchApiAnime()
-  fetchApiManga()
+  fetchApiAnime();
+  fetchApiManga();
 };
 
 // ENDPOINTS:

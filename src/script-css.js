5202fa72d8b688b7250ee234e0756f0cdc6edad8
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-08-29 21:16:31 -0300

    Cria carrossel principal dinâmicamente

--- src/script-css.js
+++ src/script-css.js
@@ -5,7 +5,37 @@ const searchDropdownButton = document.querySelector('.select-dropdown');
 const dropdownItems = document.querySelectorAll('.dropdown-item');
 const toggleSearchButton = document.querySelector('.toggle-search');
 const cancelToggleSearch = document.querySelector('.cancel-toggle-search');
+const carouselInner = document.querySelector('.carousel-inner');
 const logo = document.querySelector('.logo');
+
+const carouselContent = [
+  {
+    img: '../images/naruto-shippuden.jpg',
+    title: 'Naruto Shippuden',
+    description: 'Dia 5 de setembro estreia temporada 17',
+  },
+  {
+    img: '../images/shingeki.jpg',
+    title: 'Attack On Titan',
+    description: 'Dia 3 de Janeiro de 2022',
+  },
+  {
+    img: '../images/one-piece-wall.jpg',
+    title: 'One Piece',
+    description: 'Temporada 546 chega dia 7 de março!',
+  },
+];
+
+carouselContent.forEach(({ img, title, description }) => {
+  carouselInner.innerHTML += `<div class="carousel-item">
+    <img src="${img}" class="d-block img-carousel"/>
+    <div class="carousel-caption d-none d-md-block">
+      <h3>${title}</h3>
+      <p>${description}</p>
+    </div>
+  </div>`;
+});
+carouselInner.firstChild.classList.add('active');
 
 const selectDropdown = (event) => {
   dropdownItems.forEach((element) => {
